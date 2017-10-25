9f2182c2bc1bc76d7411cc95bc136a62a2d87795
Deniz Gurkaynak <dgurkaynak@gmail.com>
2017-10-25 10:58:43 +0300

    feat(char-rnn): Prefer tf.app in sampler

--- char-rnn/sample.py
+++ char-rnn/sample.py
@@ -7,11 +7,12 @@ from loader import BatchTextLoader
 import sqlite3
 
 
-DATA_DIR = '../training/char_rnn_20171022_112745'
-SEPERATOR_CHAR = '|'
-NUM_WOD_SAMPLE = 1
-SAVE_TO_DB = False
+tf.app.flags.DEFINE_string('data_dir', '', 'Training data directory. If empty, latest folder in training/ folder will be used')
+tf.app.flags.DEFINE_string('seperator_char', '|', 'WOD item seperator character, default `|`')
+tf.app.flags.DEFINE_integer('num_sample', 1, 'The number of WODs to be sampled, default 1')
+tf.app.flags.DEFINE_boolean('save_to_db', False, 'Should save into sqlite, default false')
 
+FLAGS = tf.app.flags.FLAGS
 
 # Helper class to access dict members with dot notation
 class dot_dict(dict):
@@ -19,96 +20,105 @@ class dot_dict(dict):
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
-# Connect to db
-connection = sqlite3.connect('../db.sqlite')
-connection.text_factory = str
-c = connection.cursor()
+def main(_):
+    # Connect to db
+    connection = sqlite3.connect('../db.sqlite')
+    connection.text_factory = str
+    c = connection.cursor()
 
-# Read options
-flags_file_path = os.path.join(DATA_DIR, 'flags.pkl')
-flags_file = open(flags_file_path, 'r')
-flags = dot_dict(pickle.load(flags_file))
-flags_file.close()
+    # If data_dir is empty, find the folder
+    if not FLAGS.data_dir:
+        dirs = [d for d in os.listdir('../training') if os.path.isdir(os.path.join('../training', d))]
+        FLAGS.data_dir = '../training/{}'.format(dirs[-1])
 
-# Read loader data
-loader_data_file_path = os.path.join(DATA_DIR, 'loader_data.pkl')
-loader_data_file = open(loader_data_file_path, 'r')
-loader_data = pickle.load(loader_data_file)
-loader_data_file.close()
+    # Read options
+    flags_file_path = os.path.join(FLAGS.data_dir, 'flags.pkl')
+    flags_file = open(flags_file_path, 'r')
+    flags = dot_dict(pickle.load(flags_file))
+    flags_file.close()
 
-# Create model
-x = tf.placeholder(tf.int32, [1, 1])
-model = CharRNNModel(flags)
-model.inference(x, training=False)
+    # Read loader data
+    loader_data_file_path = os.path.join(FLAGS.data_dir, 'loader_data.pkl')
+    loader_data_file = open(loader_data_file_path, 'r')
+    loader_data = pickle.load(loader_data_file)
+    loader_data_file.close()
+
+    # Create model
+    x = tf.placeholder(tf.int32, [1, 1])
+    model = CharRNNModel(flags)
+    model.inference(x, training=False)
 
 
-with tf.Session() as sess:
-    tf.global_variables_initializer().run()
-    saver = tf.train.Saver(tf.global_variables())
-    ckpt = tf.train.get_checkpoint_state(os.path.join(DATA_DIR, 'checkpoint'))
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        saver = tf.train.Saver(tf.global_variables())
+        ckpt = tf.train.get_checkpoint_state(os.path.join(FLAGS.data_dir, 'checkpoint'))
 
-    if ckpt and ckpt.model_checkpoint_path:
-        saver.restore(sess, ckpt.model_checkpoint_path)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
 
-        prime = SEPERATOR_CHAR
-        sampling_type=1
-        state = sess.run(model.cell.zero_state(1, tf.float32))
+            prime = FLAGS.seperator_char
+            sampling_type=1
+            state = sess.run(model.cell.zero_state(1, tf.float32))
 
-        acc = ''
-        char = prime[-1]
-        wod_i = 0
-
-        def reset_model_state():
+            acc = ''
             char = prime[-1]
+            wod_i = 0
 
-            for char in prime[:-1]:
+            def reset_model_state():
+                char = prime[-1]
+
+                for char in prime[:-1]:
+                    input = np.zeros((1, 1))
+                    input[0, 0] = loader_data.vocab[char]
+                    feed = {x: input, model.initial_state: state}
+                    [state] = sess.run([model.final_state], feed)
+
+            def weighted_pick(weights):
+                t = np.cumsum(weights)
+                s = np.sum(weights)
+                return(int(np.searchsorted(t, np.random.rand(1)*s)))
+
+            reset_model_state()
+
+            while (True):
                 input = np.zeros((1, 1))
                 input[0, 0] = loader_data.vocab[char]
                 feed = {x: input, model.initial_state: state}
-                [state] = sess.run([model.final_state], feed)
+                [prob, state] = sess.run([model.prob, model.final_state], feed)
+                p = prob[0]
 
-        def weighted_pick(weights):
-            t = np.cumsum(weights)
-            s = np.sum(weights)
-            return(int(np.searchsorted(t, np.random.rand(1)*s)))
-
-        reset_model_state()
-
-        while (True):
-            input = np.zeros((1, 1))
-            input[0, 0] = loader_data.vocab[char]
-            feed = {x: input, model.initial_state: state}
-            [prob, state] = sess.run([model.prob, model.final_state], feed)
-            p = prob[0]
-
-            if sampling_type == 0:
-                sample = np.argmax(p)
-            elif sampling_type == 2:
-                if char == ' ':
-                    sample = weighted_pick(p)
-                else:
+                if sampling_type == 0:
                     sample = np.argmax(p)
-            else:  # sampling_type == 1 default:
-                sample = weighted_pick(p)
+                elif sampling_type == 2:
+                    if char == ' ':
+                        sample = weighted_pick(p)
+                    else:
+                        sample = np.argmax(p)
+                else:  # sampling_type == 1 default:
+                    sample = weighted_pick(p)
 
-            pred = loader_data.chars[sample]
+                pred = loader_data.chars[sample]
 
-            if pred == SEPERATOR_CHAR:
-                reset_model_state()
+                if pred == FLAGS.seperator_char:
+                    reset_model_state()
 
-                acc = acc.strip()
-                if acc == '': continue
+                    acc = acc.strip()
+                    if acc == '': continue
 
-                wod_i += 1
+                    wod_i += 1
 
-                print("\n{}\n".format(acc))
-                if SAVE_TO_DB: c.execute('INSERT INTO sample (content, processed) VALUES (?, 0)', (acc,))
-                acc = ''
+                    print("\n{}\n".format(acc))
+                    if FLAGS.save_to_db: c.execute('INSERT INTO sample (content, processed) VALUES (?, 0)', (acc,))
+                    acc = ''
 
-                if wod_i == NUM_WOD_SAMPLE: break
-            else:
-                acc += pred
-                char = pred
+                    if wod_i == FLAGS.num_sample: break
+                else:
+                    acc += pred
+                    char = pred
 
-        connection.commit()
-        connection.close()
+            connection.commit()
+            connection.close()
+
+if __name__ == '__main__':
+    tf.app.run()
